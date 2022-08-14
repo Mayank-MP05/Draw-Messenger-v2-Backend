edeb7d245a18917a33b87cbfc15dcb5b685b8c86
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-14 14:06:05 +0530

    [Group Model] Get Single Group Info API implementation#2

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -11,10 +11,32 @@ router.get("/", (req, res) => {
       console.log("ERROR: Group Fetch Failed");
       return res.status(500).json({ message: "Group Fetch failed", error });
     }
-    return res.status(200).json({
-      groups: result,
-    });
+    if (result.length !== 0) {
+      const group = result[0];
+      return res.status(200).json({ group });
+    }
+    return res.status(400).json({ message: "No record found!" });
+
   });
+});
+
+router.post("/getSingleGroup", (req, res) => {
+  const { groupId } = req.body
+  Group.find({ _id: ObjectId(groupId) }, (error, result) => {
+    if (error) {
+      console.log("ERROR: Single Group Fetch Failed");
+      return res.status(500).json({ message: "Group Fetch failed", error });
+    }
+    if (result.length !== 0) {
+      const group = result[0];
+      return res.status(200).json(group);
+    }
+    return res.status(400).json({ message: "No record found!" });
+  });
+});
+
+router.get("/getMessages", (req, res) => {
+
 });
 
 router.post("/all", (req, res) => {
