33b20336641b875eff907c9b0e8b97f61825dcd7
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-14 13:53:16 +0530

    Socket IO Server added on port 9001

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+
+// CORS Error fixes
+const cors = require("cors");
+app.use(cors());
+
+const { Server } = require("socket.io");
+const http = require('http');
+const server = app.listen(9001);
+const io = new Server(server, { cors: { origin: '*' } });
+
 const port = 9000;
 const connection = require("./db/db-connect");
 const user = require("./models/user.model");
 const group = require("./models/group.model");
+
+// Bodyparser Middleware
+const bodyParser = require("body-parser");
 app.use(bodyParser.json({}));
+
+io.on('connection', (socket) => {
+  console.log('[INIT] Socket.io Connected!');
+  socket.on('chat', (payload, callback) => {
+    console.log(payload);
+    io.emit('chat', { ...payload, fromServer: true })
+    // callback("All Good from Server!");
+  })
+});
 
 // IMPORTS: Controllers Import
 const UserController = require("./controllers/users.controller");
